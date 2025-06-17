1065c489d84d026b4cd3e70b94eda1b2bd2d33a9
andres martinez <amartinez@loyaltysci.com>
2025-06-17 11:52:48 -0500

    Initial Commit

---
+++ Prueba-interRapidisimo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prueba-interRapidisimo
+//
+//  Created by Andres Martinez gonzalez on 17/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
